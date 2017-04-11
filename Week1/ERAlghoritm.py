116e2771bd6e74c4206ce58e61428b69df29bffa
Home <cc@ubuntu.ubuntu-domain>
2017-04-11 17:45:08 +0300

    Added ER algo

---
+++ Week1/ERAlghoritm.py
@@ -0,0 +1,140 @@
+import numpy as np
+n=10
+p=1
+np.linspace(0,n-1,1)
+np.linspace(0,n-1,num=1)
+np.linspace(0,9,num=1)
+np.linspace(2.0, 3.0, num=5)
+np.linspace(0,n-1 ,n)
+V=np.linspace(0,n-1,n)#the edges  or   the vertixes
+V
+E={}
+for i in range(0,len(V)-):
+    for i in range(i+1,len(V)):
+        print(i,v
+        )
+for i in range(0,len(V)-1):
+    for i in range(i+1,len(V)):
+        print(i,v
+        )
+for i in range(0,len(V)-1):
+    for i in range(i+1,len(V)):
+        print(i,j)
+for i in range(0,len(V)-):
+    for j in range(i+1,len(V)):
+        print(i,v
+        )
+for i in range(0,len(V)-1):
+    for j in range(i+1,len(V)):
+        print(i,j)
+range(0:1)
+range(0,1)
+np.random_sample(1)
+from  numpy import  random
+random.random_sample(1)
+random.random_sample(1)
+random.random_sample(1)
+random.random_sample(1)
+random.random_sample(1)
+E={}
+E[1]=(1,2)
+E
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations([0,1,2,3], 2):
+    for  s in itertools.combinations([0,1,2,3], 2):
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations(list(V),2):
+        a=np.random_sample(1)
+        if a<p:
+            E[A]=s
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations(list(V),2):
+        a=np.random_sample(1)
+        if a<p:
+            E[A]=s
+return (E,V)
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations(list(V),2):
+        a=np.random_sample(1)
+        if a<p:
+            E[A]=s
+    return (E,V)
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations(list(V),2):
+        a=np.random_sample(1)
+        if a<p:
+            E[A]=s
+    return (E,V)
+ER(10,1)
+import itertools
+ER(10,1)
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations(list(V),2):
+        a=random.random_sample(1)
+        if a<p:
+            E[A]=s
+    return (E,V)
+ER(10,1)
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations(list(V),2):
+        a=random.random_sample(1)
+        if a<p:
+            E[a]=s
+    return (E,V)
+ER(10,1)
+random.random_sample(1)
+int(random.random_sample(1))
+random.random_sample(1)
+str(random.random_sample(1))
+def ER(n,p):
+    import numpy as np
+    from numpy import  random
+    V=np.linspace(0,n-1,n)
+    E={}
+    for  s in itertools.combinations(list(V),2):
+        a=random.random_sample(1)
+        if a<p:
+            E[str(a)]=s
+    return (E,V)
+ER(10,1)
+ER(10,0)
+ER(10,1)
+E,V=ER(10,1)
+E
+V
+len(E)
+3/7
+from __future__ import division
+2.7
+3/7
+dict={0: 0.125, 1: 0.125, 2: 0.25, 3: 0.375, 4: 0.125}
+sum(dict.values())
+%history -f ERAlghoritm.py
